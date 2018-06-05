10e30062118ae344460921300635071fa169eff3
nizan <nizanfs@gmail.com>
2018-06-05 15:34:51 +0300

    Finalize game including checks

--- xo.py
+++ xo.py
@@ -4,6 +4,7 @@ import string
 board_size = 3
 all_letters = string.ascii_lowercase
 
+
 def start_game():
 
     players = gather_players_names()
@@ -24,11 +25,10 @@ def start_game():
 
 
 def gather_players_names():
-    return ["nizan1", "nizan2"]
-    # player_1_name = get_console_input("Player1, what's your name?", 3, 20)
-    # player_2_name = get_console_input("Player2, what's your name?", 3, 20)
-    #
-    # return [player_1_name, player_2_name]
+    player_1_name = get_console_input("Player1, what's your name?", 3, 20)
+    player_2_name = get_console_input("Player2, what's your name?", 3, 20)
+
+    return [player_1_name, player_2_name]
 
 
 def get_console_input(message, min_length, max_length):
@@ -49,18 +49,65 @@ def main_game_logic(players):
     win = False
     while game_moves < max_moves and not win:
         print_board(board)
-        player_move(board, players, current_player)
-        win = check_board_win(board)
+        slot_location = player_move(board, players, current_player)
+        win = check_board_win(board, slot_location)
 
-        game_moves += 1
-        current_player = 1-current_player
+        if not win:
+            game_moves += 1
+            current_player = 1-current_player
 
     print_board(board)
-    print('Game Ended')
+
+    if win:
+        end_data = "{} won the game".format(players[current_player])
+    else:
+        end_data = "It's a DRAW!"
+
+    print('GAME ENDED - {}\n\n'.format(end_data))
 
 
-def check_board_win(board):
-    return False
+def check_board_win(board, slot_location):
+    row = slot_location[0]
+    column = slot_location[1]
+    current_letter = board[row][column]
+    # check main crosses
+    if slot_location[0] == slot_location[1]:
+        win = True
+        # check left top to right bottom
+        for index in range(board_size):
+            if board[index][index] != current_letter:
+                win = False
+                break
+
+        if win:
+            return True
+
+        # check left bottom to right top
+        win = True
+        for index in range(board_size):
+            if board[index][board_size-index-1] != current_letter:
+                win = False
+                break
+        if win:
+            return True
+
+    # check column
+    win = True
+    for row_entry in board:
+        if row_entry[column] != current_letter:
+            win = False
+            break
+    if win:
+        return True
+
+    # check row
+    win = True
+    for entry in board[row]:
+        if entry != current_letter:
+            win = False
+            break
+
+    return win
 
 
 def print_board(board):
@@ -86,18 +133,17 @@ def print_board(board):
 def player_move(board, players, current_player):
     print()
     # get slot
-    valid_slot = False
     slot_letter = "X"
     if current_player == 1:
         slot_letter = "O"
 
-    while not valid_slot:
+    while True:
         chosen_slot = get_console_input(players[current_player] + " choose your slot", 2, 2).lower()
         # check that slot is valid and available
         slot_location = get_slot_coordinates(board_size, chosen_slot)
-        if slot_location is not None and board[slot_location[1]][slot_location[0]] == " ":
-            board[slot_location[1]][slot_location[0]] = slot_letter
-            valid_slot = True
+        if slot_location is not None and board[slot_location[0]][slot_location[1]] == " ":
+            board[slot_location[0]][slot_location[1]] = slot_letter
+            return slot_location
         else:
             print('Chosen slot input is invalid! try again please')
 
@@ -106,10 +152,10 @@ def get_slot_coordinates(board_max, slot_string):
     column = ord(slot_string[0]) - 97
     row = int(slot_string[1]) - 1
 
-    if column < 0 or column > board_max or row < 0 or row > board_max:
+    if column < 0 or column >= board_max or row < 0 or row >= board_max:
         return None
 
-    return [column, row]
+    return [row, column]
 
 
 start_game()
