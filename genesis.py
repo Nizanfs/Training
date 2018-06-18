aba8b498b80abeda5b905edf8ce77fc3da8cd6f6
nizan <nizanfs@gmail.com>
2018-06-18 15:24:30 +0300

    Last fixes

--- genesis.py
+++ genesis.py
@@ -5,41 +5,46 @@ def file_gen(filename):
 
 
 def read_words(filename):
-    gen = file_gen(filename)
+    file_gen_wrapper = type('', (object,), {})()
+
+    def init_file_gen():
+        file_gen_wrapper.file_gen = file_gen(filename)
+
+    init_file_gen()
 
     def find_next_match(search_value):
-        matched_result = next(gen)
+        matched_result = next(file_gen_wrapper.file_gen)
         while not matched_result.startswith(search_value):
-            matched_result = next(gen)
+            matched_result = next(file_gen_wrapper.file_gen)
 
         return matched_result
 
     try:
-        val = next(gen)
+        val = next(file_gen_wrapper.file_gen)
         while True:
             val = (yield val)
             if val is not None:
 
                 try:
-                    gen = file_gen(filename)
+                    init_file_gen()
                     next_val = find_next_match(val)
                     val = next_val
                 except StopIteration:
-                    gen = file_gen(filename)
+                    init_file_gen()
                     first_letter = val.lower()[0]
                     # Skipping to the next relevant letter.
                     next_letter = chr(ord(first_letter) + 1)
                     val = find_next_match(next_letter)
 
             else:
-                val = next(gen)
+                val = next(file_gen_wrapper.file_gen)
 
     except StopIteration:
         pass
 
 
 if __name__ == '__main__':
-    words = read_words('../words')
+    words = read_words('./words')
     print(next(words))
     print(next(words))
     print(next(words))
