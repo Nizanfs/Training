00503bb954f0fa481a197ce30fb81735336094c5
nizan <nizanfs@gmail.com>
2018-06-19 13:02:19 +0300

    additional tests

--- test_game_of_dbs.py
+++ test_game_of_dbs.py
@@ -28,6 +28,34 @@ def test_get_device_histogram(setup_data):
     assert histogram[0]['protocol'] == protocol
 
 
+def test_get_device_status(setup_data):
+    ip_addr_base = '10.10.20.'
+    protocol = 'HTTPS'
+    data = []
+    num_of_ips = 10
+    num_of_timestamps = 3
+    for i in range(num_of_ips):
+        ip_addr = ip_addr_base + str(i)
+        for j in range(num_of_timestamps):
+            timestamp = datetime.now()
+            data.append(Entry(str(ipaddr.IPAddress(ip_addr)), protocol=protocol, timestamp=timestamp))
+
+    implement_me.index(data)
+    status = implement_me.get_devices_status()
+    assert len(status) == num_of_ips
+    # Check last inserted ip and timestamp
+    last_inserted_ip_and_timestamp_returned = status[-1]
+    last_inserted_ip_and_timestamp = data[-1]
+    assert last_inserted_ip_and_timestamp_returned[0] == last_inserted_ip_and_timestamp.ip
+    assert last_inserted_ip_and_timestamp_returned[1] == last_inserted_ip_and_timestamp.timestamp
+    
+    # Check last inserted timestamp for first ip
+    first_inserted_ip_returned_status = status[0]
+    first_inserted_ip_data = data[num_of_timestamps - 1]
+    assert first_inserted_ip_returned_status[0] == first_inserted_ip_data.ip
+    assert first_inserted_ip_returned_status[1] == first_inserted_ip_data.timestamp
+
+
 def test_adding_and_fetching_s1wide(setup_data):
     data = datasets.s1wide()
     check_data(data, 128)
