f5b62b655d86c9f996234e71b87753217a4ce027
nizan <nizanfs@gmail.com>
2018-06-20 14:15:25 +0300

    remove redundant code

--- elastic_handler.py
+++ elastic_handler.py
@@ -8,11 +8,10 @@ ELASTIC_PORT = f'9200'
 def start_elastic():
     client = docker.from_env()
     container = client.containers.run('elasticsearch', ports={ELASTIC_PORT: ELASTIC_PORT}, detach=True, remove=True)
-    # Stop the code execution until the elastic-docker is up.
+    # Wait for elastic container to start.
     logs = container.logs(stream=True, tail=2)
     while not (next(logs).endswith(b'started\n')):
         time.sleep(1)
-        logs = container.logs(stream=True, tail=2)
 
     return container
 
